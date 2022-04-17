b676a92a6a06daf857b079b73be6646d4b4e711e
DenilsonMelo <denilsonmlo42@gmail.com>
2022-04-17 13:54:59 -0300

    added project styles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,35 @@
+import React from 'react';
+import { Container, MovieList, Movie } from "./styles";
+
 function Home(){
+
+  const movies = [
+    {
+      title: 'Spider Man',
+      image_url: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+    },
+    {
+      title: 'X Man - Evolution',
+      image_url: 'https://upload.wikimedia.org/wikipedia/pt/thumb/0/00/Spider-Man_No_Way_Home_poster.jpg/250px-Spider-Man_No_Way_Home_poster.jpg'
+    }
+  ]
+
   return(
-    <>
-      <h1>Hello World!!</h1>
-    </>
+    <Container>
+      <h1>Filmes Populares</h1>
+      <MovieList>
+
+        {movies.map(movie => {
+          return(
+            <Movie>
+              <img src={movie.image_url} alt={movie.image_url}/>
+              <span>{movie.title}</span>
+            </Movie>
+          )
+        })}
+
+      </MovieList>
+    </Container>
   );
 }
 
